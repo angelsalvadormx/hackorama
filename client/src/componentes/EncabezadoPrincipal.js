fe845abb6261820e7bd65ec9d81eadc28e64e10b
Jorge Vargas <jvargas18@ucol.mx>
2019-11-02 17:59:25 -0600

    Change style n login pages

---
+++ client/src/componentes/EncabezadoPrincipal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EncabezadoPrincipal = () => {
+  return (
+    <div className="d-flex flex-column align-items-center h-25 pt-5">
+      <h1>Logo</h1>
+      <h3>Nombre App</h3>
+    </div>
+  );
+};
+
+export default EncabezadoPrincipal;
