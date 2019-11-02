06d37923e252428e3c481e1681ed95fa6383b26e
Angel Salvador <02angelsalvador@gmail.com>
2019-11-02 04:44:18 -0600

    cambio clase nav

--- client/src/componentes/Menu.js
+++ client/src/componentes/Menu.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const menu = () => {
   return (
-    <nav className="navbar navbar-expand-lg fixed-bottom navbar-light bg-light">
+    <nav className="navbar navbar-expand-sm fixed-bottom navbar-light bg-light">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
