ffcd4734a1853c6f60d63309e9af9b8f462c0b80
Jorge Vargas <jvargas18@ucol.mx>
2019-11-02 04:52:17 -0600

    Modify App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import M from 'materialize-css';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //  import Paginas
-import Principal from './paginas/Principal';
-import InicioSesion from './paginas/InicioSesion';
-import RecuperarCuenta from './paginas/RecuperarCuenta';
-import Registro from './paginas/Registro';
-import Inicio from './paginas/Inicio';
-import ComparacionMascotas from './paginas/ComparacionMascotas';
-import Publicaciones from './paginas/Publicaciones';
-import Publicacion from './paginas/Publicacion';
-import AgregarPublicacionMenu from './paginas/AgregarPublicacionMenu';
-import AgregarPublicacionFormulario from './paginas/AgregarPublicacionFormulario';
-import Perfil from './paginas/Perfil';
+import Principal from "./paginas/Principal";
+import InicioSesion from "./paginas/InicioSesion";
+import RecuperarCuenta from "./paginas/RecuperarCuenta";
+import Registro from "./paginas/Registro";
+import Inicio from "./paginas/Inicio";
+import ComparacionMascotas from "./paginas/ComparacionMascotas";
+import Publicaciones from "./paginas/Publicaciones";
+import Publicacion from "./paginas/Publicacion";
+import AgregarPublicacionMenu from "./paginas/AgregarPublicacionMenu";
+import AgregarPublicacionFormulario from "./paginas/AgregarPublicacionFormulario";
+import Perfil from "./paginas/Perfil";
 
 function App() {
-    M.AutoInit();
-  
   return (
     <BrowserRouter>
       <Switch>
@@ -31,8 +28,16 @@ function App() {
         <Route exac path="/comparacion" component={ComparacionMascotas} />
         <Route exac path="/publicaciones" component={Publicaciones} />
         <Route exac path="/publicacion/:id" component={Publicacion} />
-        <Route exac path="/agregar-publicacion" component={AgregarPublicacionMenu} />
-        <Route exac path="/agregar-publicacion/:id" component={AgregarPublicacionFormulario} />
+        <Route
+          exac
+          path="/agregar-publicacion"
+          component={AgregarPublicacionMenu}
+        />
+        <Route
+          exac
+          path="/agregar-publicacion/:id"
+          component={AgregarPublicacionFormulario}
+        />
         <Route exac path="/perfil/:id" component={Perfil} />
       </Switch>
     </BrowserRouter>
--- client/src/paginas/Principal.js
+++ client/src/paginas/Principal.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const Principal = ()=>{
-  return(
-    <h1>Principal</h1>
-  );
-};
-export default Principal;
+export default class Principal extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container h-100 bg-dark">
+          <div>logo</div>
+          <h1 className="d-flex justify-content-center">Nombre App</h1>
+          <div className="row d-flex justify-content-center">
+            <div className="col-12 col-lg-6">
+              <div className="form-group">
+                <Link
+                  to="/inicio-sesion"
+                  className="btn btn-secondary btn-block"
+                >
+                  Iniciar Sesion
+                </Link>
+              </div>
+              <div className="form-group">
+                <Link to="/registro" className="btn btn-secondary btn-block">
+                  Crear Cuenta
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
