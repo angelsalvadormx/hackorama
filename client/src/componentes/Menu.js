aaef35e8203d5fe5170992e7def50583afd95932
Angel Salvador <02angelsalvador@gmail.com>
2019-11-02 04:24:46 -0600

    update css tema e index.html nuevo script (iconos)

--- client/src/componentes/Menu.js
+++ client/src/componentes/Menu.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const menu = () => {
   return (
-    <nav className="nav-extended fixed bottom">
-      <div className="nav-content">
-        <ul className="tabs tabs-transparent">
-          <li className="tab"><a href="#test1">Test 1</a></li>
-          <li className="tab"><a className="active" href="#test2">Test 2</a></li>
-          <li className="tab disabled"><a href="#test3">Disabled Tab</a></li>
-          <li className="tab"><a href="#test4">Test 4</a></li>
-        </ul>
+    <nav class="navbar navbar-expand-lg fixed-bottom navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <Link to="/productos" className="nav-item nav-link active"><i class="fas fa-map-marker-alt"></i></Link>
+          <Link to="/productos" className="nav-item nav-link">Buscador</Link>
+        </div>
       </div>
     </nav>
   );
