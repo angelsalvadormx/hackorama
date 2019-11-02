53c04c65aa7c81f0c64dacd9ddfd9fa1f951c0e4
Angel Salvador <02angelsalvador@gmail.com>
2019-11-02 16:30:16 -0600

    vista formulario resportar mascota terminado

--- client/src/paginas/AgregarPublicacionFormulario.js
+++ client/src/paginas/AgregarPublicacionFormulario.js
@@ -6,8 +6,20 @@ export default class AgregarPublicacionFormulario extends Component {
     const tipo_form = estado == 1 ? 'Perdida' : estado == 2 ? 'Rescatado' : estado == 3 ? 'Sin hogar' : '';
     return (
       <Fragment>
-        <h3 className="w-100">Reportar mascota <br /> {tipo_form}</h3>
-
+        <header className="w-100 text-center pb-5 pt-5">
+          <h3>Reportar mascota <br /> {tipo_form}</h3>
+        </header>
+        <form className="h-100 d-flex flex-column pr-4 pl-4">
+          <input type="date" required className="border shadow p-3 mb-5 bg-white rounded" placeholder="Fecha" />
+          <input type="text" required className="border shadow p-3 mb-5 bg-white rounded" placeholder="Nombre" />
+          <input type="text" required className="border shadow p-3 mb-5 bg-white rounded" placeholder="Color" />
+          <textarea required className="border shadow p-3 mb-5 bg-white rounded" placeholder="Descipción"></textarea>
+          <footer className="mb-5 w-100 d-flex justify-content-around">
+            <button type="button" className="w-45 p-4 shadow btn btn-light">Añadir Ubicación</button>
+            <button type="button" className="w-45 p-4 shadow btn btn-light position-relative btn-file"><input type="file" className="position-absolute w-100 h-100" />Añadir Foto</button>
+          </footer>
+          <button type="submit" className="rounded-10 pt-2 pb-2 btn btn-primary">Enviar</button>
+        </form>
       </Fragment>
     );
   };
