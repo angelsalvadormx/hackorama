855cd13e042a9a6fe80e97edf17dfb91e5865544
Angel Salvador <02angelsalvador@gmail.com>
2019-11-02 18:36:45 -0600

    404 no encontrado, pagina terminada

--- client/src/paginas/NoEncontrada.js
+++ client/src/paginas/NoEncontrada.js
@@ -1,6 +1,14 @@
 import React from "react";
-
+import './noEncontrado/404.css';
+import dog from './noEncontrado/404.gif'
 function NoEncontrada() {
-  return <h1>404: Not Found</h1>;
+  return (
+    <main id="noEncontrado" className="position-relative w-100 h-100 bkg-img" style={{ backgroundImage: `url(${dog})` }}>
+      <section className="msg404 text-center position-absolute">
+        <h2 className="mb-1">404</h2>
+        <small>Página no encontrada.</small>
+      </section>
+    </main>
+  );
 }
 export default NoEncontrada;
