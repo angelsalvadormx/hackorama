8f96a784fc05261a7e8326c8c0301d019f2d1d7f
Angel Salvador <02angelsalvador@gmail.com>
2019-11-02 19:38:28 -0600

    animacion menu

--- client/src/componentes/Menu.js
+++ client/src/componentes/Menu.js
@@ -5,15 +5,15 @@ const menu = ({ btnActivo }) => {
   return (
     <nav className="border-top pt-2 pb-2 fixed-bottom bg-light d-flex justify-content-center">
       <div className="w-75 d-flex justify-content-between ">
-        <Link to="/inicio" className={"text-dark text-center" + (btnActivo == 'permisos' ? 'active-menu' : '')}>
+        <Link to="/inicio" className={"btn-menu text-center" + (btnActivo == 'permisos' ? ' active-menu' : '')}>
           <i className="fas fa-paw"></i>
-          <small className="w-100 d-block text-dark">Perdidos</small>
+          <small className="w-100 d-block ">Perdidos</small>
         </Link>
-        <Link to="/comparacion" className={"text-dark text-center" + (btnActivo == 'rescatados' ? 'active-menu' : '')}>
+        <Link to="/comparacion" className={"btn-menu text-center" + (btnActivo == 'rescatados' ? ' active-menu' : '')}>
           <i className="fas fa-paw"></i>
           <small className="w-100 d-block">Rescatados</small>
         </Link>
-        <Link to="/productos" className={"text-dark text-center" + (btnActivo == 'sinHogar' ? 'active-menu' : '')}>
+        <Link to="/productos" className={"btn-menu text-center" + (btnActivo == 'sinHogar' ? ' active-menu' : '')}>
           <i className="fas fa-paw"></i>
           <small className="w-100 d-block">Sin hogar</small>
         </Link>
