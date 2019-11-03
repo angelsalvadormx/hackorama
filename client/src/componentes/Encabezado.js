c059c6ce3dc4ab92fcf98b02c30099ce7a3380d1
Victor Alfonso Lechuga Illingworth <lechuga.alf@gmail.com>
2019-11-02 18:03:07 -0600

    fixed deps

--- client/src/componentes/Encabezado.js
+++ client/src/componentes/Encabezado.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 // active -->
 
 const Encabezado = () => {
   return (
     <div className="pos-f-t">
       <nav className="navbar navbar-light">
-        <button className="outline-none pl-0 pr-0 navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="outline-none pl-0 pr-0 navbar-toggler border-0"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarToggleExternalContent"
+          aria-controls="navbarToggleExternalContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <h2>Find Me</h2>
-        <Link to="/agregar-publicacion-menu" className="text-dark"><i className="fas fa-plus"></i></Link>
-        <div className="collapse navbar-collapse" id="navbarToggleExternalContent">
-          <div class="navbar-nav pb-3">
-            <a class="nav-item nav-link " href="/perfil/"><i class="fas fa-cog"></i> Perfil</a>
-            <a class="nav-item nav-link" href="#"> <i class="fas fa-sign-out-alt"></i> Cerrar sesión</a>
+        <Link to="/agregar-publicacion-menu" className="text-dark">
+          <i className="fas fa-plus"></i>
+        </Link>
+        <div
+          className="collapse navbar-collapse"
+          id="navbarToggleExternalContent"
+        >
+          <div className="navbar-nav pb-3">
+            <a className="nav-item nav-link " href="/perfil/">
+              <i className="fas fa-cog"></i> Perfil
+            </a>
+            <a className="nav-item nav-link" href="#">
+              {" "}
+              <i className="fas fa-sign-out-alt"></i> Cerrar sesión
+            </a>
           </div>
         </div>
       </nav>
--- client/src/paginas/Publicaciones.js
+++ client/src/paginas/Publicaciones.js
@@ -1,92 +1,105 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 // Import componentes
-import Menu from '../componentes/Menu';
-import Encabezado from '../componentes/Encabezado';
-import Mapa from '../componentes/Mapa';
+import Menu from "../componentes/Menu";
+import Encabezado from "../componentes/Encabezado";
+import Mapa from "../componentes/Mapa";
 
 const Publicaciones = () => {
   const json = [
     {
-      "id": 1,
-      "fecha": "23 oct 2019",
-      "nombre": "Nombre",
-      "raza": "Raza",
-      "color": "Color",
-      "descripcion": "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
-      "imagen": "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
+      id: 1,
+      fecha: "23 oct 2019",
+      nombre: "Nombre",
+      raza: "Raza",
+      color: "Color",
+      descripcion: "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
+      imagen:
+        "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
     },
     {
-      "id": 2,
-      "fecha": "23 oct 2019",
-      "nombre": "Nombre",
-      "raza": "Raza",
-      "color": "Color",
-      "descripcion": "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
-      "imagen": "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
+      id: 2,
+      fecha: "23 oct 2019",
+      nombre: "Nombre",
+      raza: "Raza",
+      color: "Color",
+      descripcion: "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
+      imagen:
+        "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
     },
     {
-      "id": 3,
-      "fecha": "23 oct 2019",
-      "nombre": "Nombre",
-      "raza": "Raza",
-      "color": "Color",
-      "descripcion": "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
-      "imagen": "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
+      id: 3,
+      fecha: "23 oct 2019",
+      nombre: "Nombre",
+      raza: "Raza",
+      color: "Color",
+      descripcion: "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
+      imagen:
+        "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
     },
     {
-      "id": 4,
-      "fecha": "23 oct 2019",
-      "nombre": "Nombre",
-      "raza": "Raza",
-      "color": "Color",
-      "descripcion": "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
-      "imagen": "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
+      id: 4,
+      fecha: "23 oct 2019",
+      nombre: "Nombre",
+      raza: "Raza",
+      color: "Color",
+      descripcion: "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
+      imagen:
+        "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
     },
     {
-      "id": 5,
-      "fecha": "23 oct 2019",
-      "nombre": "Nombre",
-      "raza": "Raza",
-      "color": "Color",
-      "descripcion": "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
-      "imagen": "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
+      id: 5,
+      fecha: "23 oct 2019",
+      nombre: "Nombre",
+      raza: "Raza",
+      color: "Color",
+      descripcion: "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
+      imagen:
+        "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
     },
     {
-      "id": 6,
-      "fecha": "23 oct 2019",
-      "nombre": "Nombre",
-      "raza": "Raza",
-      "color": "Color",
-      "descripcion": "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
-      "imagen": "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
+      id: 6,
+      fecha: "23 oct 2019",
+      nombre: "Nombre",
+      raza: "Raza",
+      color: "Color",
+      descripcion: "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
+      imagen:
+        "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
     },
     {
-      "id": 7,
-      "fecha": "23 oct 2019",
-      "nombre": "Nombre",
-      "raza": "Raza",
-      "color": "Color",
-      "descripcion": "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
-      "imagen": "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
-    },    {
-      "id": 8,
-      "fecha": "23 oct 2019",
-      "nombre": "Nombre",
-      "raza": "Raza",
-      "color": "Color",
-      "descripcion": "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
-      "imagen": "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
+      id: 7,
+      fecha: "23 oct 2019",
+      nombre: "Nombre",
+      raza: "Raza",
+      color: "Color",
+      descripcion: "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
+      imagen:
+        "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
+    },
+    {
+      id: 8,
+      fecha: "23 oct 2019",
+      nombre: "Nombre",
+      raza: "Raza",
+      color: "Color",
+      descripcion: "AJKAJAJAJAJAJFASDFKASDKFKASDFKASKDFK",
+      imagen:
+        "https://i2.wp.com/www.silocreativo.com/wp-content/uploads/2017/12/visual-code-portada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
     }
-
   ];
 
-  const todoItems = json.map(item =>
-    <Link to={"/publicacion/" + item.id} className="w-45 ht-100 mb-3 d-block">
+  const todoItems = json.map((item, key) => (
+    <Link to={"/publicacion/" + item.id} key={key} className="w-45 ht-100 mb-3 d-block">
       <section className="shadow-lg p-2 bg-white w-100 rounded-10 d-flex h-100">
-        <picture className="d-block w-25 h-100 bkg-img rounded-10" style={{ backgroundImage: "url(" + item.imagen + ")" }} ></picture>
+        <picture
+          className="d-block w-25 h-100 bkg-img rounded-10"
+          style={{ backgroundImage: "url(" + item.imagen + ")" }}
+        ></picture>
         <main className="w-50 pr-2 pl-2 text-dark">
-          <label className="h5 d-block mb-1"><strong>{item.nombre}</strong></label>
+          <label className="h5 d-block mb-1">
+            <strong>{item.nombre}</strong>
+          </label>
           <label className="h6 mb-2 d-block">{item.raza}</label>
           <label className="mb-2">{item.color}</label>
         </main>
@@ -95,14 +108,13 @@ const Publicaciones = () => {
         </aside>
       </section>
     </Link>
-  );
+  ));
   return (
     <React.Fragment>
-      <Encabezado/>
+      <Encabezado />
       <main className="container pb-5">
-        <div className="w-100">
-          {todoItems}
-        </div>
+        <Mapa/>
+        <div className="w-100">{todoItems}</div>
       </main>
       <Menu />
     </React.Fragment>
